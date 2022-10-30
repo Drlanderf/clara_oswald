24669eb2e031072b2445037c3e2a3ee2c72e99a9
Lukas Paquay <lukas.paquay@student.hepl.be>
2022-10-30 20:51:35 +0100

    💡- Add or update comments in source code : *WARNING CLIENT EVERYTIME THE LAST* 🐛 - Finally fix the auto-reply when says "quoi"

--- src/events/client/messageCreate.js
+++ src/events/client/messageCreate.js
@@ -8,7 +8,7 @@ module.exports =
          * @param {import("../../bot.js")} client
          * @param {Message} message
          */
-        async execute(message,client) {
+        async execute(message,client) { // WARNING CLIENT EVERYTIME THE LAST
             console.log("Event messageCreate successfully apply");
             console.log("message.content is :");
             console.log(message.content);
@@ -22,24 +22,5 @@ module.exports =
             } catch (error){
                 console.error(error);
             }
-
-
-            /*if (message.content === 'quoi'){
-                message.channel.send("yolo")
-                console.log("Enter into message create if");
-            }*/
-
-            /*try{
-                console.log("Enter into message create try case");
-                let test = message.content;
-                let result = test.toLowerCase();
-                if(result.includes(MyTestingReplyVar))
-                    client.reply("feur :stuck_out_tongue_closed_eyes: !",)
-                        .then(() =>{
-                            console.log("Reply Action 'feur' SUCCEED !");
-                        });
-            } catch (error) {
-                console.error(error);
-            }*/
         }
     }
