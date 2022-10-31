7a2630a83001329d612434d460f2121774888410
Lukas Paquay <lukas.paquay@student.hepl.be>
2022-10-31 13:37:52 +0100

    🚑️ - Hotfix handleCommands.js

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -1,17 +1,16 @@
-const { REST } = require('@discordjs/rest');
+const { REST } = require('discord.js/rest');
 const { Routes } = require('discord-api-types/v9');
 const fs = require('fs');
 require('dotenv').config();
 const clientId = process.env.CLIENT_ID;
 const guildId = process.env.GUILD_ID;
+const rest = new REST({ version: '9' }).setToken(process.env.token);
 module.exports = (client) =>
 {
-    client.handleCommands = async () =>
-    {
+    client.handleCommands = async () => {
         console.log('Handler of Commands successfully apply');
         const commandFolders = fs.readdirSync('./src/commands');
-        for (const folder of commandFolders)
-        {
+        for (const folder of commandFolders) {
             const commandFiles = fs
                 .readdirSync(`./src/commands/${folder}`)
                 .filter((file)=> file.endsWith('.js'));
@@ -24,14 +23,12 @@ module.exports = (client) =>
                 console.log(`Command : ${command.data.name} has been passed through the handler`);
             }
         }
-        const rest = new REST({ version: "9" }).setToken(process.env.token);
         try  {
             console.log("Started refreshing application (/) commands.");
-
-            await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
-                body: client.commandArray,
-            })
-
+            await rest.put(
+                Routes.applicationGuildCommands(clientId, guildId),
+                {body: client.commandArray,}
+            );
             console.log("Successfully reloaded application (/) commands.")
         } catch (error) {
             console.error(error);
