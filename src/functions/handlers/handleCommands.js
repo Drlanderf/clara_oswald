01c415bcd9299443349100567dc2691039b2c9b7
Lukas Paquay <lukas.paquay@student.hepl.be>
2022-10-31 13:23:12 +0100

    🚑️ - Hotfix handleCommands.js

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -28,7 +28,7 @@ module.exports = (client) =>
         try  {
             console.log("Started refreshing application (/) commands.");
 
-            await rest.put(Routes.applicationCommands(clientId, guildId), {
+            await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
                 body: client.commandArray,
             })
 
