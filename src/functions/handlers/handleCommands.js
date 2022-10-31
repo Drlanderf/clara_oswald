3ce9f98ff5f23e9e965687cfbcfd65944c8fd5f6
Lukas Paquay <lukas.paquay@student.hepl.be>
2022-10-31 15:17:11 +0100

    🚑️ - Trying to remove duplicate commands

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -10,14 +10,15 @@ module.exports = (client) =>
 {
     client.handleCommands = async () => {
         console.log('Handler of Commands successfully apply');
+        await rest.put(Routes.applicationCommands(clientId), { body: [] })
+        await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
         const commandFolders = fs.readdirSync('./src/commands');
         for (const folder of commandFolders) {
             const commandFiles = fs
                 .readdirSync(`./src/commands/${folder}`)
                 .filter((file)=> file.endsWith('.js'));
             const { commands, commandArray} = client;
-            for (const file of commandFiles)
-            {
+            for (const file of commandFiles) {
                 const command = require(`../../commands/${folder}/${file}`);
                 await commands.set(command.data.name, command);
                 commandArray.push(command.data.toJSON());
