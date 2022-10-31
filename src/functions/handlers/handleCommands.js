a88b9c7de12e50f978ac27cbff112ab4a61ee41b
Lukas Paquay <lukas.paquay@student.hepl.be>
2022-10-31 13:53:24 +0100

    🚑️ - Hotfix handleCommands.js

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -1,5 +1,4 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const { REST,Routes } = require('discord.js');
 const fs = require('fs');
 require('dotenv').config();
 const clientId = process.env.CLIENT_ID;
