d0e6e0265d713e094e04ebc734c51d8a4adb66a0
Lukas Paquay <lukas.paquay@student.hepl.be>
2022-10-31 12:28:08 +0100

    🧑‍💻- Change the login log

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -3,6 +3,13 @@ module.exports = {
         once: true,
         async execute(client) {
                 setInterval(client.pickPresence, 10 * 1000);
-                console.log(`${client.user.tag} has logged into Discord !`);
+                console.log(`o--------------------------------------------o`);
+                console.log(`|                                            |`);
+                console.log(`|           Thanks to use my bot             |`);
+                console.log(`|                                            |`);
+                console.log(`|          Logged into Discord as            |`);
+                console.log(`|            ${client.user.tag}              |`);
+                console.log(`|                                            |`);
+                console.log(`o--------------------------------------------o`);
         },
     };
