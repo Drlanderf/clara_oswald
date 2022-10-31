57e0f332803068cfce44e1956a3a1c0dfe83ffbc
Lukas Paquay <lukas.paquay@student.hepl.be>
2022-10-31 10:52:56 +0100

    ✨- Added the usage of .env VARIABLE

--- src/events/client/guildMemberRemove.js
+++ src/events/client/guildMemberRemove.js
@@ -1,8 +1,10 @@
 const Discord = require("discord.js");
-const MyLeavingMessage01 = " est parti sans un aurevoir.";
-const MyLeavingMessage02 = " a voyagé dans le passé où il n'était pas encore sur le serveur !";
-const MyLeavingMessage03 = " a rejoint la planete prison des Dalek, on ne l'a plus jamais revu.";
-const MyLeavingMessage04 = " est parti en quête d'aventure, on ne l'a plus jamais revu.";
+require('dotenv').config();
+const MyWelcomeChannelID = process.env.JOIN_CHANNEL;
+const MyLeavingMessage01 = process.env.MY_LEAVING_MESSAGE00;
+const MyLeavingMessage02 = process.env.MY_LEAVING_MESSAGE01;
+const MyLeavingMessage03 = process.env.MY_LEAVING_MESSAGE02;
+const MyLeavingMessage04 = process.env.MY_LEAVING_MESSAGE03;
 //Actif quand une personne quitte le serveur discord.
 module.exports =
     {
@@ -15,13 +17,13 @@ module.exports =
             console.log("Event guildMemberRemove successfully apply");
 
 
-            const WelcomeChannel = client.channels.cache.get("762448607260835850");
+            const WelcomeChannel = client.channels.cache.get(`${MyWelcomeChannelID}`);
             try{
                 let Randomizer = Math.floor(Math.random() * 101);
-                if(Randomizer>=75 && Randomizer<=100) WelcomeChannel.send("<@"+ member.id + ">" + MyLeavingMessage01);
-                if(Randomizer>=50 && Randomizer<75) WelcomeChannel.send("<@"+ member.id + ">" + MyLeavingMessage02);
-                if(Randomizer>=25 && Randomizer<50) WelcomeChannel.send("<@"+ member.id + ">" + MyLeavingMessage03);
-                if(Randomizer>=0 && Randomizer<25) WelcomeChannel.send("<@"+ member.id + ">" + MyLeavingMessage04);
+                if(Randomizer>=75 && Randomizer<=100) WelcomeChannel.send("<@"+ member.id + ">" + `${MyLeavingMessage01}`);
+                if(Randomizer>=50 && Randomizer<75) WelcomeChannel.send("<@"+ member.id + ">" + `${MyLeavingMessage02}`);
+                if(Randomizer>=25 && Randomizer<50) WelcomeChannel.send("<@"+ member.id + ">" + `${MyLeavingMessage03}`);
+                if(Randomizer>=0 && Randomizer<25) WelcomeChannel.send("<@"+ member.id + ">" + `${MyLeavingMessage04}`);
             } catch (error){
                 console.log(error);
             }
