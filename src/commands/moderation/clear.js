9ea31e40e2d6d8086a7e6f13853d92172f0f564f
Lukas Paquay <lukas.paquay@student.hepl.be>
2022-11-01 18:09:42 +0100

    🐛 Fix bug

--- src/commands/moderation/clear.js
+++ src/commands/moderation/clear.js
@@ -5,7 +5,7 @@ module.exports = {
     .setDescription("Suprime X message dans le salon courrant.")
     .addIntegerOption((option) =>
       option
-        .setName(`X`)
+        .setName(`nombre`)
         .setDescription(
           `Nombre.s que vous souhaitez suprimer d'un coup. (max 100)`
         )
@@ -13,17 +13,17 @@ module.exports = {
     ),
   async execute(interaction, client) {
     console.log("Command clear successfully apply");
-    let x = interaction.options.getInteger(`X`);
+    let number = interaction.options.getInteger(`nombre`);
 
     if (x >= 1 && x >= 100) {
       await interaction.channel.bulkDelete(x);
       interaction.reply({
-        content: `${x} message.s ont été suprimé.s!`,
+        content: `${number} message.s ont été suprimé.s!`,
         ephemeral: true,
       });
     } else {
       interaction.reply({
-        content: `${x} n'est pas inclus entre 1 et 100`,
+        content: `${number} n'est pas inclus entre 1 et 100`,
         ephemeral: true,
       });
     }
