a88f1efab4d6a6c158f7cdf7cd9941927454bf19
landerf <lukas.paquay@outlook.be>
2022-11-02 13:08:52 +0100

    ⏪️🚧 - Revert change about Logged into Discord as

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -4,5 +4,10 @@ module.exports = {
     setInterval(client.pickPresence, 30 * 1000);
     setTimeout(client.checkVideoTech, 30 * 1000); // ?
     setTimeout(client.checkVideoGaming, 30 * 1000); // ?
+    console.log(`o--------------------------------------------o`);
+    console.log(`|          Logged into Discord as            |`);
+    console.log(`|            ${client.user.tag}               |`);
+    console.log(`o--------------------------------------------o`);
   },
+
 };
