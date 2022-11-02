4550a7dc0fb37ccaad523919f64002089f466ef2
landerf <lukas.paquay@outlook.be>
2022-11-02 12:34:49 +0100

    ⏪️🚧 - Revert changes, set interval spam, settimeout make it cringe

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -3,14 +3,14 @@ module.exports = {
   once: true,
   async execute(client) {
     setInterval(client.pickPresence, 30 * 1000);
-    setTimeout(client.checkVideoTech, 30 * 1000);// ?
-    setTimeout(client.checkVideoGaming, 30 * 1000);// ?
+    setTimeout(client.checkVideoTech, 30 * 1000); // ?
+    setTimeout(client.checkVideoGaming, 30 * 1000); // ?
     console.log(`o--------------------------------------------o`);
     console.log(`|                                            |`);
     console.log(`|          Logged into Discord as            |`);
     console.log(`|                                            |`);
     console.log(`|            ${client.user.tag}               |`);
     console.log(`|                                            |`);
-    console.log(`o--------------------------------------------o`)
+    console.log(`o--------------------------------------------o`);
   },
 };
