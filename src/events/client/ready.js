1f0f9e03fd622c2e0d2775aafbaa20de09408f22
landerf <lukas.paquay@outlook.be>
2022-11-02 13:46:49 +0100

    🐛🚧 - Trying to fix checkVideos bugs

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -2,8 +2,8 @@ module.exports = {
   name: "ready",
   async execute(client) {
     setInterval(client.pickPresence, 30 * 1000);
-    setTimeout(client.checkVideoTech, 30 * 1000); // ?
-    setTimeout(client.checkVideoGaming, 30 * 1000); // ?
+    setInterval(client.checkVideoTech, 30 * 1000); // ?
+    setInterval(client.checkVideoGaming, 30 * 1000); // ?
     console.log(`o--------------------------------------------o`);
     console.log(`|          Logged into Discord as            |`);
     console.log(`|            ${client.user.tag}               |`);
