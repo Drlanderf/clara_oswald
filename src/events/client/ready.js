38a275c927e98121dec00f514b193c4d70f9ec9f
landerf <lukas.paquay@outlook.be>
2022-11-02 12:17:21 +0100

    🐛🚧 - Fixed setTimeout into setInterval

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -3,8 +3,8 @@ module.exports = {
   once: true,
   async execute(client) {
     setInterval(client.pickPresence, 30 * 1000);
-    setInterval(client.checkVideoTech, 30 * 1000);
-    setInterval(client.checkVideoGaming, 30 * 1000);
+    setTimeout(client.checkVideoTech, 30 * 1000);
+    setTimeout(client.checkVideoGaming, 30 * 1000);
     console.log(`o--------------------------------------------o`);
     console.log(`|                                            |`);
     console.log(`|          Logged into Discord as            |`);
