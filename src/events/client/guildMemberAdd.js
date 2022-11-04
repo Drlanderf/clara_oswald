b41ab6a352e7fa9aa43a9f21d497efd2adf06076
landerf <lukas.paquay@outlook.be>
2022-11-05 00:05:32 +0100

    🐛🚧guildMemberAdd canvas

--- src/events/client/guildMemberAdd.js
+++ src/events/client/guildMemberAdd.js
@@ -1,11 +1,25 @@
 const Canvas = require("@napi-rs/canvas");
-const { AttachmentBuilder, Discord } = require("discord.js");
-const { request } = require("undici");
+const Discord = require("discord.js");
 const MyWelcomeChannelID = process.env.JOIN_CHANNEL;
 const MyRoleID00 = process.env.ROLE_ID00;
 const MyRoleID01 = process.env.ROLE_ID01;
 const MyRoleID02 = process.env.ROLE_ID02;
 const MyCustomWelcomeMessage = process.env.CUSTOM_WELCOME_MESSAGE;
+
+const welcomeCanvas = {};
+welcomeCanvas.create = Canvas.createCanvas(1024, 500);
+welcomeCanvas.context = welcomeCanvas.create.getContext("2d");
+welcomeCanvas.context.font = "68px sans-serif";
+welcomeCanvas.context.fillStyle = "#ffffff";
+Canvas.loadImage(`${process.cwd()}/assets/img/bg.png`).then(async (img) => {
+  welcomeCanvas.context.drawImage(img, 0, 0, 1024, 500);
+  welcomeCanvas.context.fillText("Bienvenue", 350, 75);
+  welcomeCanvas.context.beginPath();
+  welcomeCanvas.context.arc(512, 245, 128, 0, Math.PI * 2, true);
+  welcomeCanvas.context.stroke();
+  welcomeCanvas.context.fill();
+});
+
 module.exports = {
   name: "guildMemberAdd",
   /**
@@ -14,62 +28,37 @@ module.exports = {
    */
   async execute(member, client) {
     console.log("Event guildMemberAdd successfully apply");
-    const canvas = Canvas.createCanvas(700, 250);
-    const context = canvas.getContext("2d");
-    const background = await Canvas.loadImage(
-      `${process.cwd()}/assets/img/tardis.png`
+    const welcomeChannel = client.channels.cache.get(`${MyWelcomeChannelID}`);
+
+    let canvas = welcomeCanvas;
+    canvas.context.font = "42px sans-serif";
+    canvas.context.textAlign = "center";
+    canvas.context.fillText(member.user.tag.toUpperCase(), 512, 425);
+    canvas.context.font = "28px sans-serif";
+    canvas.context.fillText(
+      `Tu es le ${member.guild.memberCount}e membres`,
+      512,
+      475
     );
-    context.drawImage(background, 0, 0, canvas.width, canvas.height);
-    context.strokeStyle = "#191246";
-    context.strokeRect(0, 0, canvas.width, canvas.height);
-    const { body } = await request(
-      member.user.displayAvatarURL({ extension: "jpg" })
-    );
-    const avatar = await Canvas.loadImage(await body.arrayBuffer());
+    canvas.context.beginPath();
+    canvas.context.arc(512, 245, 119, 0, Math.PI * 2, true);
+    canvas.context.closePath();
+    canvas.context.clip();
 
-    const applyText = (canvas, text) => {
-      const context = canvas.getContext("2d");
-      let fontSize = 70;
-
-      do {
-        context.font = `${(fontSize -= 10)}px sans-serif`;
-      } while (context.measureText(text).width > canvas.width - 300);
-      return context.font;
-    };
-
-    context.drawImage(avatar, 25, 25, 200, 200);
-    context.beginPath();
-    context.arc(125, 125, 100, 0, Math.PI * 2, true);
-    context.closePath();
-    context.clip();
-
-    context.font = "28px sans-serif";
-    context.fillStyle = "#ffffff";
-    context.fillText("Bienvenue", canvas.width / 2.5, canvas.height / 3.5);
-
-    context.font = applyText(canvas, `${member.user.displayName}!`);
-    context.fillStyle = "#ffffff";
-    context.fillText(
-      `${member.user.displayName}!`,
-      canvas.width / 2.5,
-      canvas.height / 1.8
-    );
-
-    const attachment = new AttachmentBuilder(await canvas.encode("png"), {
-      name: "profile-image.png",
+    await Canvas.loadImage(
+      member.user.displayAvatarURL({ size: 1024, format: "png" })
+    ).then((img) => {
+      canvas.context.drawImage(img, 393, 125, 238, 238);
     });
-    const welcomeChannel = member.guild.channels.cache.get(
-      `${MyWelcomeChannelID}`
-    );
+
+      const attachment = new Discord.AttachmentBuilder(await canvas.encode('png'), { name: `welcome-${member.id}.png` });
 
     try {
       welcomeChannel.send({
         content: `:wave::skin-tone-2: Salutation ${member},\n${MyCustomWelcomeMessage}`,
         files: [attachment],
       });
-      await member.roles.add(`${MyRoleID00}`);
-      await member.roles.add(`${MyRoleID01}`);
-      await member.roles.add(`${MyRoleID02}`);
+      await member.roles.add([`${MyRoleID00}`,`${MyRoleID01}`,`${MyRoleID02}`]);
     } catch (error) {
       console.log(error);
     }
