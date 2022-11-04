b2b65414f2bde75675ff7e352af28dc567e77199
landerf <lukas.paquay@outlook.be>
2022-11-04 01:14:16 +0100

    ⚡️improve performances

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -2,8 +2,8 @@ module.exports = {
   name: "ready",
   async execute(client) {
     setInterval(client.pickPresence, 15 * 1000);
-    setInterval(client.checkVideoTech, 300 * 1000); // ?
-    setInterval(client.checkVideoGaming, 300 * 1000); // ?
+    setInterval(client.checkVideoTech, 300 * 1000); //Improve perf
+    setInterval(client.checkVideoGaming, 300 * 1000); //Improve perf
     console.log(`o--------------------------------------------o`);
     console.log(`|          Logged into Discord as            |`);
     console.log(`|            ${client.user.tag}               |`);
