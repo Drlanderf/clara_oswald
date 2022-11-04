4894ab19d1751130e75ccb40288801476a5e82b0
landerf <lukas.paquay@outlook.be>
2022-11-04 20:22:12 +0100

    🎨Improve structure / format of the code.

--- src/events/client/guildMemberAdd.js
+++ src/events/client/guildMemberAdd.js
@@ -1,5 +1,5 @@
 const canvacord = require("canvacord");
-const { AttachmentBuilder,Discord} = require('discord.js');
+const { AttachmentBuilder, Discord } = require("discord.js");
 require("dotenv").config();
 const MyWelcomeChannelID = process.env.JOIN_CHANNEL;
 const MyRoleID00 = process.env.ROLE_ID00;
@@ -14,7 +14,10 @@ module.exports = {
    */
   async execute(member, client) {
     console.log("Event guildMemberAdd successfully apply");
-    let welcomeCard = new canvacord.Welcomer(textTitle = "Bienvenue",textMessage = "Bienvenue sur {server}" )
+    let welcomeCard = new canvacord.Welcomer(
+      (textTitle = "Bienvenue"),
+      (textMessage = "Bienvenue sur {server}")
+    )
       .setUsername(member.user.username)
       .setDiscriminator(member.user.discriminator)
       .setAvatar(member.user.displayAvatarURL({ format: "png" }))
@@ -29,16 +32,21 @@ module.exports = {
       .setColor("avatar", "#191246")
       .setBackground(`${process.cwd()}/assets/img/tardis.png`)
       .setMemberCount(member.guild.memberCount);
-      welcomeCard.textTitle = `Bienvenue`;
-      welcomeCard.textMessage = `Bienvenue sur ${member.guild.name}`
-      let Attachment = new AttachmentBuilder(await welcomeCard.build(), "welcome.png");
-    const welcomeChannel = member.guild.channels.cache.get(`${MyWelcomeChannelID}`);
+    welcomeCard.textTitle = `Bienvenue`;
+    welcomeCard.textMessage = `Bienvenue sur ${member.guild.name}`;
+    let Attachment = new AttachmentBuilder(
+      await welcomeCard.build(),
+      "welcome.png"
+    );
+    const welcomeChannel = member.guild.channels.cache.get(
+      `${MyWelcomeChannelID}`
+    );
 
     try {
-        welcomeChannel.send({
-            content: `:wave::skin-tone-2: Salutation ${member},\n${MyCustomWelcomeMessage}`,
-            files: [Attachment],
-        });
+      welcomeChannel.send({
+        content: `:wave::skin-tone-2: Salutation ${member},\n${MyCustomWelcomeMessage}`,
+        files: [Attachment],
+      });
       await member.roles.add(`${MyRoleID00}`);
       await member.roles.add(`${MyRoleID01}`);
       await member.roles.add(`${MyRoleID02}`);
