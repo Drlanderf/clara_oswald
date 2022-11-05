9852f0b23db565b6b127c6a265bc98573f3055a4
landerf <lukas.paquay@outlook.be>
2022-11-05 18:26:16 +0100

    🐛 mongoDB log bugfix

--- src/events/mongo/connecting.js
+++ src/events/mongo/connecting.js
@@ -1,6 +1,6 @@
 const chalk = require("chalk");
 module.exports = {
-    name: "connected",
+    name: "connecting",
     execute() {
         console.log(chalk.cyan("[Database Status]: Connecting..."));
     },
--- src/events/mongo/err.js
+++ src/events/mongo/err.js
@@ -1,6 +1,6 @@
 const chalk = require("chalk");
 module.exports = {
-    name: "connected",
+    name: "err",
     execute(err) {
         console.log(chalk.red(`An error occurred with the database connection:\n${err}`));
     },
