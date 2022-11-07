3925bbc4166c6e68690ab2c0e5cdc903b9cd6245
landerf <lukas.paquay@outlook.be>
2022-11-07 10:18:19 +0100

    🚧Disabling video and stream checks following a bug

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,11 +1,9 @@
 const ascii = require("ascii-table");
 const { loadCommands } = require("../../functions/handlers/handleCommands");
 const { checkDBGuildId } = require("../../functions/mongo/checkDBGuildId");
-const {
-  checkStreamTwitch,
-} = require("../../functions/social/checkStreamTwitch");
-const { checkVideoGaming } = require("../../functions/social/checkVideoGaming");
-const { checkVideoTech } = require("../../functions/social/checkVideoTech");
+//const {checkStreamTwitch} = require("../../functions/social/checkStreamTwitch");
+//const { checkVideoGaming } = require("../../functions/social/checkVideoGaming");
+//const { checkVideoTech } = require("../../functions/social/checkVideoTech");
 const { pickPresence } = require("../../functions/tools/pickPresence");
 module.exports = {
   name: "ready",
@@ -17,8 +15,8 @@ module.exports = {
     loadCommands(client).then();
 
     setInterval(() => pickPresence(client), 15 * 1000);
-    setInterval(() => checkVideoTech(client), 15 * 1000);
-    setInterval(() => checkVideoGaming(client), 15 * 1000);
-    setInterval(() => checkStreamTwitch(client), 60 * 1000);
+    //setInterval(() => checkVideoTech(client), 60 * 1000);
+    //setInterval(() => checkVideoGaming(client), 60 * 1000);
+    //setInterval(() => checkStreamTwitch(client), 120 * 1000);
   },
 };
