5d275763d49bc9f1ea35d94669dde579b28778b8
Hakunek <hakunekpl@gmail.com>
2022-11-13 10:20:11 +0100

    overhaul of clear command: added two more options - user and channel added recursion so users can give number larger than 100 included checking if the messages are older than 2 weeks as those cannot be removed using bulkDelete
    added little change in handleCommands.js
    so commands already in JSON format can be used

--- src/commands/moderation/clear.js
+++ src/commands/moderation/clear.js
@@ -1,23 +1,95 @@
-const { SlashCommandBuilder } = require("discord.js");
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("clear")
-        .setDescription("Delete a number of messages in the current channel.")
-        .addIntegerOption((option) => option.setName(`number`).setDescription(`Number.s you want to delete at once. (max 100)`).setRequired(true)),
-    async execute(interaction, client) {
-        let number = interaction.options.getInteger(`number`);
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-        if (number >= 1 && number <= 100) {
-            await interaction.channel.bulkDelete(number);
-            interaction.reply({
-                content: `${number} posts have been deleted!`,
-                ephemeral: true,
+async function timesRepeat(toDelete) {
+    if (toDelete < 100) return [toDelete];
+    let i = [100];
+    while (toDelete >= 100) {
+        toDelete -= 100;
+        if (toDelete > 100) i.push(100);
+        else i.push(toDelete);
+    }
+    return i;
+}
+
+function plural(i) {
+    if (i == 1) return "message";
+    return "messages";
+}
+
+async function loopFunction(i, deleting, channel, user, total, interaction) {
+    let numb = deleting[deleting.length - i];
+    if (numb != 0) {
+        let messages = await channel.messages.fetch({
+            limit: parseInt(numb),
+        });
+        if (messages?.size == 0)
+            return interaction.editReply({
+                content: `Got rid of: ${total} ${plural(total)} ${user ? `that ${user} sent` : ``}`,
             });
+        let final;
+        if (user) {
+            let one = [...(await messages.filter((m) => m.author.id == user.id))].map((e) => e[1]);
+            final = one.slice(0, numb);
         } else {
-            interaction.reply({
-                content: `${number} is not included between 1 and 100`,
-                ephemeral: true,
-            });
+            final = messages;
         }
+        channel
+            .bulkDelete(final, true)
+            //true after `final` is needed as it makes the djs check if messages that you want to delete are older than 2 weeks or cannot be deleted by bot and skips them
+            .then((messages) => {
+                total += messages?.size || 0;
+            })
+            .catch((error) => {
+                console.error("error →", error);
+                interaction.editReply({
+                    content: `There was an error, check the console/tell the Waves to do so ^-^" ${channel}!`,
+                });
+            });
+    }
+    //waiting 1 second between each iteration so we don't get flagged for spamming to djs api
+    await sleep(1000);
+    if (!--i || !numb) loopFunction(i, deleting, channel, user, total);
+    else {
+        return interaction.editReply({
+            content: `Got rid of: ${total} messages ${plural(total)} ${user ? `that ${user} sent` : ``} ${channel.name}`,
+        });
+    }
+}
+
+module.exports = {
+    data: {
+        name: "clear",
+        description: "Remove specific number of messages",
+        options: [
+            {
+                name: "amount",
+                description: "Amount of messages to purge",
+                type: 4,
+                min_value: 1,
+                required: true,
+            },
+            {
+                name: "targetuser",
+                description: "User whose messages are to be purged",
+                type: 6,
+                required: false,
+            },
+            {
+                name: "targetchannel",
+                description: "Channel where messages are to be purged",
+                type: 7,
+                channel_types: [0, 2, 5, 10, 11, 12],
+                required: false,
+            },
+        ],
+    },
+    async execute(interaction, client) {
+        const user = interaction.options.getUser("targetuser");
+        let toDelete = interaction.options.getInteger("amount");
+        await interaction.deferReply({ ephemeral: true });
+        const total = 0;
+        const deleting = await timesRepeat(toDelete);
+        const channel = (await interaction.options.getChannel("targetchannel")) || (await client.channels.cache.get(interaction.channelId));
+        loopFunction(deleting.length, deleting, channel, user, total, interaction);
     },
 };
--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -1,27 +1,28 @@
 async function loadCommands(client) {
-  const fs = require("fs");
-  const ascii = require("ascii-table");
+    const fs = require("fs");
+    const ascii = require("ascii-table");
 
-  const table = new ascii().setHeading("Commands", "Status");
+    const table = new ascii().setHeading("Commands", "Status");
 
-  await client.commands.clear();
+    await client.commands.clear();
 
-  client.commandsArray = [];
+    client.commandsArray = [];
 
-  const commandFolders = fs.readdirSync("./src/commands");
+    const commandFolders = fs.readdirSync("./src/commands");
 
-  for (const folder of commandFolders) {
-    const commandFiles = fs
-      .readdirSync(`./src/commands/${folder}`)
-      .filter((file) => file.endsWith(".js"));
-    for (const file of commandFiles) {
-      const command = require(`../../commands/${folder}/${file}`);
-      await client.commands.set(command.data.name, command);
-      client.commandsArray.push(command.data.toJSON());
-      table.addRow(command.data.name, `🟩`);
+    for (const folder of commandFolders) {
+        const commandFiles = fs.readdirSync(`./src/commands/${folder}`).filter((file) => file.endsWith(".js"));
+        for (const file of commandFiles) {
+            const command = require(`../../commands/${folder}/${file}`);
+            await client.commands.set(command.data.name, command);
+
+            //added checking if toJSON is a function so commands that are already supplied within JSON format can also be used
+            if (typeof command.data.toJSON == "function") client.commandsArray.push(command.data.toJSON());
+            else client.commandsArray.push(command.data);
+            table.addRow(command.data.name, `🟩`);
+        }
     }
-  }
-  await client.application.commands.set(client.commandsArray);
-  return console.log(table.toString(), "\nCommands Loaded");
+    await client.application.commands.set(client.commandsArray);
+    return console.log(table.toString(), "\nCommands Loaded");
 }
 module.exports = { loadCommands };
