82cee8f85289f9bc604fcd8ced7f6f9e21419953
Hakunek <hakunekpl@gmail.com>
2022-11-13 10:57:41 +0100

    made package.json file look as it should included repo urls in package.json
    removed most partials
    but why the heck did I remove them?
    partials are objects that are not included in cache
    And as till now there is no feature that
    would benefit from having access to partial information
    https://en.wikipedia.org/wiki/You_aren%27t_gonna_need_it

--- src/bot.js
+++ src/bot.js
@@ -1,14 +1,12 @@
 require("dotenv").config({ path: `${__dirname}/../.env` });
 const { BOT_TOKEN, DATABASE_TOKEN } = process.env;
-const { Client, Collection, Partials } = require("discord.js");
-const { User, Message, GuildMember, ThreadMember } = Partials;
+const { Client, Collection } = require("discord.js");
 const { connect } = require("mongoose");
 const { loadEvents } = require("../src/functions/handlers/handleEvents");
 
 const client = new Client({
     intents: 3276799,
-    partials: [User, Message, GuildMember, ThreadMember],
-}); // => That will include ALL intents/partials needed to work
+}); // => That will include ALL intents needed to work
 console.log(`o----------------------------o`);
 console.log(`|   Thanks for using my bot     |`);
 client.configs = new Collection();
@@ -17,7 +15,7 @@ client.commands = new Collection();
 (async () => {
     try {
         loadEvents(client);
-        await connect(DATABASE_TOKEN);
+        connect(DATABASE_TOKEN);
         client.login(BOT_TOKEN);
     } catch (e) {
         console.error(e);
