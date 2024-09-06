d3dbf502f290865470e0d6ecfbd4631190de5189
lukas <lukas.paquay@outlook.be>
2024-09-06 21:49:42 +0200

    🚧 Updating Distube

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ const { BOT_TOKEN, DATABASE_TOKEN } = process.env;
 const { Client, Collection } = require("discord.js");
 const { connect,mongoose } = require("mongoose");
 const { loadEvents } = require("./functions/handlers/handleEvents");
-const { DisTube } = require('distube')
+const { DisTube } = require('distube');
+import { SoundCloudPlugin } from "@distube/soundcloud";
+import { SpotifyPlugin } from "@distube/spotify";
+import { YouTubePlugin } from "@distube/youtube";
+
 
 const client = new Client({
   intents: [process.argv[3] ? 3276799 : 531,`GuildVoiceStates`]
@@ -16,10 +20,9 @@ client.events = new Collection();
 client.commands = new Collection();
 client.buttons = new Collection();
 client.modals = new Collection();
-client.distube = new DisTube(client, {
-  emitNewSongOnly: true,
-  leaveOnFinish: true,
-  emitAddSongWhenCreatingQueue: false,
+client.distube = new Distube.default(client, {
+  emptyCooldown: 30,
+  plugins: [new SoundCloudPlugin(), new SpotifyPlugin(),new YouTubePlugin()]
 });
 
 
